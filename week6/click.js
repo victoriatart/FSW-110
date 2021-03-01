1e68b7bc9aadcdcfb649c277e5974ab9549bfb56
victoriatart <68974738+victoriatart@users.noreply.github.com>
2021-03-01 00:54:29 -0500

    Delete week6 directory

--- week6/click.js
+++
@@ -1,5 +0,0 @@
-// make the box disapear when the user clicks it
-var box = document.getElementsByClassName("red-box");
-box[0].addEventListener('click', (e) => {
-    e.target.style.display = "none"
-})
